76668c2784437edaa8c33dba9756526edb4593ba
Lars Berger <lars.berger@yahoo.com>
2024-01-01 17:05:17 +0000

    feat: add prettier plugin for astro

--- prettier/index.js
+++ prettier/index.js
@@ -1,16 +1,17 @@
 /**
  * Explicitly define config options that can be overriden by an .editorconfig
- *
  * Ref: https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+ *
+ * @type {import("prettier").Config}
  */
 module.exports = {
   endOfLine: 'lf',
-  trailingComma: "all",
-  arrowParens: "avoid",
+  trailingComma: 'all',
+  arrowParens: 'avoid',
   semi: true,
   useTabs: false,
   singleQuote: true,
   tabWidth: 2,
   printWidth: 75,
-  plugins: ['prettier-plugin-packagejson'],
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-astro'],
 };
